802de032919010a6b309ebe4f21ae54fca0eff4c
Daniel <adm.dcf@gmail.com>
2023-01-05 21:45:25 -0300

    Fix publish

--- publishconf.py
+++ publishconf.py
@@ -7,20 +7,17 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://theostuff.github.io/'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
 
-FAVICON = "/favicon.ico"
-SITENAME = "Foo Bar's Blog"
-SITETITLE = "Foo Bar"
-SITESUBTITLE = "Web Developer"
+
